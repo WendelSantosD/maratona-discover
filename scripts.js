b64b7b58702b9793d2646dc8d608315595dca115
WendelSantosD <wendelwcsantos@gmail.com>
2021-01-30 01:22:09 -0300

    Aula 03 1:33:10

--- scripts.js
+++ scripts.js
@@ -22,25 +22,25 @@ const transactions = [
     {
     id: 1,
     description: 'Luz',
-    amount: -50000,
+    amount: -50001,
     date: '23/01/2021',
     },
     {
     id: 2,
     description: 'Website',
-    amount: 50000,
+    amount: 500000,
     date: '23/01/2021',
     },
     {
     id: 3,
     description: 'Internet',
-    amount: -20000,
+    amount: -20012,
     date: '23/01/2021',
     },
     {
     id: 3,
     description: 'App',
-    amount: 20000,
+    amount: 200000,
     date: '23/01/2021',
     }
 ]
@@ -52,13 +52,25 @@ const transactions = [
 
 const Transaction = {
     incomes(){
-        // somar as entradas
+        let income = 0
+        transactions.forEach(transaction => {
+            if (transaction.amount > 0) {
+                income += transaction.amount
+            }
+        })
+        return income
     },
     expenses(){
-        // somar as saídas
+        let expense = 0
+        transactions.forEach(transaction => {
+            if (transaction.amount < 0) {
+                expense += transaction.amount
+            }
+        })
+        return expense
     },
     total(){
-        //entradas - saídas
+        return Transaction.incomes() + Transaction.expenses()
     }
 }
 
@@ -74,18 +86,50 @@ const DOM = {
     },
     innerHTMLTransaction(transaction) {
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
         const html = `
             <td class="description">${transaction.description}</td>
-            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
                 <img src="./assets/minus.svg" alt="Remover transação">
             </td>
         `
         return html
+    },
+    updateBalance() {
+        document.getElementById('incomeDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.incomes())
+
+        document.getElementById('expenseDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.expenses())
+
+        document.getElementById('totalDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.total())
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0  ? "-" : ""
+        
+        value = String(value).replace(/\D/g, "")
+
+        value= Number(value) / 100
+        
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+        
+        return signal + value
     }
 }
 
 transactions.forEach(function (transaction) {
     DOM.addTransaction(transaction)    
 })
+
+DOM.updateBalance()
